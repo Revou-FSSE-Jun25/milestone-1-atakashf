0cfa15f2d193dad409c819331e13a444f18a4b50
Ataka Shafia <shafiaataka@gmail.com>
2025-07-31 21:50:00 +0700

    feat: enhance portfolio layout and add responsiv

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const navLinks = document.querySelectorAll('.nav-link');
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".nav-link");
 
-  navLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
       event.preventDefault();
-      const targetId = event.currentTarget.getAttribute('href');
-      
-      const targetElement = (targetId === '#home') ? document.body : document.querySelector(targetId);
-      
+      const targetId = event.currentTarget.getAttribute("href");
+
+      const targetElement =
+        targetId === "#home" ? document.body : document.querySelector(targetId);
+
       if (targetElement) {
         targetElement.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
+          behavior: "smooth",
+          block: "start",
         });
       }
     });
   });
 
-  // --- Contact Form Submission Alert ---
-  const contactForm = document.getElementById('contact-form');
+  // --- Hamburger ---
+  const hamburger = document.querySelector(".hamburger");
+  const navList = document.querySelector(".nav-list");
 
-  contactForm.addEventListener('submit', (event) => {
-    event.preventDefault(); 
-    
+  hamburger.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
+
+  // --- Contact Form Submission Alert ---
+  const contactForm = document.getElementById("contact-form");
+
+  contactForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
     // Show a success message
-    alert('Thank you for your message! I will get back to you soon.');
-    
+    alert("Thank you for your message! I will get back to you soon.");
+
     // Clear the form fields
     contactForm.reset();
   });
